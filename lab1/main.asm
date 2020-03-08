c4da6cf7063bd9c456e624ae0f5484ec2199ffcc
Alpha-Girl <alphaboy@mail.ustc.edu.cn>
2020-03-08 11:27:33 +0800

    Success

--- lab1/main.asm
+++ lab1/main.asm
@@ -20,46 +20,43 @@ sort:
         move $s2,$a0     #  $a0=v
         move $s3,$a1     #  $s3=n
         move $s0,$s3   #  $s0=i=n
-        addi  $s0,-1   #   i=n-1
+        addi $s0,$s0,-1   #   i=n-1
 forlist1:
         slt  $t0,$zero,$s0    # if(0 < i) $t0=1 else $t0=0
-        beq  $t0,$zero,exit1 
-        addi $s1,$s3,0     # $s1=j=i
+        beq  $t0,$zero,exit0 
+        li   $s1,1     # $s1=j=0
         move $t2,$a0       # $t2=v
 forlist2:
-        slt  $t0,$s0,$s1      # if(j < n) $t0=1 else $t0=0
-        bne  $t0,$zero,exit2
-        move $t2,
+        slt  $t0,$s0,$s1      # if(j < i) $t0=1 else $t0=0
+        bne  $t0,$zero,exit1
         lw   $t3,0($t2)
         lw   $t4,4($t2)
-        slt  $t0,$t4,$t3
+        slt  $t0,$t4,$t3      # if(a[j+1]<a[j]) $t0=1 else $t0=0
         beq  $t0,$zero,exit2
-        move $a0,$s2
-        move $a1,$s1
-        jal  swap
-        addi $s1,$s1,-1
-        j    forlist2
+        sw   $t3,4($t2)
+        sw   $t4,0($t2)
+        
 exit2:
-        addi $s0,$s0,1
-        j    forlist1
+        addi $s1,$s1,1       #  j++
+        addi $t2,$t2,4       #  v+=4
+        j    forlist2
 exit1:
-        lw   $s0,0($sp)
-        lw   $s1,4($sp)
-        lw   $s2,8($sp)
-        lw   $s3,12($sp)
-        lw   $ra,16($sp)
-        addi $sp,$sp,20
-        jr   $ra
-swap:
-        sll  $t1,$a1,2    # $a0
-        add  $t1,$a0,$s1
+        addi $s0,$s0,-1
+        j    forlist1
 
-        lw   $t0,0($t1)
-        lw   $t2,4($t1)
-
-        sw   $t2,0($t1)
-        sw   $t0,4($t1)
-
-        jr   $ra
+exit0:  
+        move $s0,$t7        # i=n
+        addi $s0,$s0,-1
+        move $t2,$a0        # $t2 =v        
+for2:   
+        slt  $t0,$s0,$zero    # if(0 < i) $t0=1 else $t0=0
+        bne  $t0,$zero,last   # if(0=i) jump to end
+        li   $v0,1
+        lw   $a0,0($t2)
+        syscall
+        addi $t2,$t2,4
+        addi $s0,$s0,-1
+        j    for2
+last:    
 
     
